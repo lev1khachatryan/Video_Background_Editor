0d96dd73a72944be10f992283e7c1919a554a16e
lev1khachatryan <levon.khachatryan.1996.db@gmail.com>
2020-02-02 11:00:45 +0400

    story / Change of background with grabCut

--- python/bg_fg_conbiner.py
+++ python/bg_fg_conbiner.py
@@ -1,3 +1,6 @@
+# USAGE
+# python bg_fg_combiner.py
+
 from __future__ import print_function
 import numpy as np
 import cv2
@@ -7,10 +10,20 @@ import os
 # Find OpenCV version
 (MAJOR_VER, MINOR_VER, SUBMINOR_VER) = (cv2.__version__).split('.')
 
-def bg_subtractor(path):
-	# Open Video
-	cap = cv2.VideoCapture(cv2.samples.findFileOrKeep(path))
+def bgSubtractor(path):
+	'''
+	Estimate and extract the background of a scene when the camera is static 
+	and there are some moving objects in the scene.
+	------------------------
+	Parameters
+		path (string): path of the video from which we want to extract the background
+	Returns
+		nd.array: background of the video
+	------------------------
+	'''
 
+	# Open the video and check if it successfully done
+	cap = cv2.VideoCapture(cv2.samples.findFileOrKeep(path))
 	if not cap.isOpened:
 	    print('Unable to open: ' + path)
 	    exit(0)
@@ -29,24 +42,37 @@ def bg_subtractor(path):
 	median_frame = np.median(frames, axis=0)
 	# cv2.imwrite('background.jpg', median_frame)
 	
-	# When everything done, release the capture
+	# When everything done, cleanup the camera and close any open windows
 	cap.release()
 	cv2.destroyAllWindows()
 
 	return median_frame
 
 
-def fg_bg_combiner(path, bg_frame, save_dir):
+def fgBgCombiner(path, bg_frame, save_dir):
+	'''
+	Change background in videos using GrabCut algorithm
+	------------------------
+	Parameters
+		path (string): path of the video to change the background
+		bg_frame (array like): background image (or nd.array)
+		save_dir (string): path where to save obtained video
+	Returns
+		None
+	------------------------
+	'''
+
+	# Get video name from full path
 	file_name = path.split('/')[-1].split('.')[0]
 
-	# Open video and check if it successfully opened
+	# Open the video and check if it successfully done
 	cap = cv2.VideoCapture(cv2.samples.findFileOrKeep(path))
 	if not cap.isOpened:
 	    print('Unable to open: ' + path)
 	    exit(0)
 
 	# Get fps and shape of the video
-	if int(MAJOR_VER)  < 3:
+	if int(MAJOR_VER) < 3:
 		fps = cap.get(cv2.cv.CV_CAP_PROP_FPS)
 	else:
 		fps = cap.get(cv2.CAP_PROP_FPS)
@@ -56,31 +82,40 @@ def fg_bg_combiner(path, bg_frame, save_dir):
 	fourcc = cv2.VideoWriter_fourcc(*'XVID')
 	out = cv2.VideoWriter(save_dir + file_name + '.avi', fourcc, fps, size)
 
+	# Resize background frame (image) to video shape
 	background = cv2.resize(bg_frame, size, interpolation=cv2.INTER_AREA)
-
+	
+	# Loop over the frames of the video
 	while True:
+		# Grab the current frame
 	    _, frame = cap.read()
+	    # If the frame could not be grabbed, then we have reached the end of the video
 	    if frame is None:
 	    	break
 
+	    # Convert it to grayscale
 	    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+	    # Initial mask
 	    mask = np.zeros(frame.shape[:2], np.uint8)
+	    # Background and foreground models for grabCut
 	    bgdModel = np.zeros((1, 65), np.float64)
 	    fgdModel = np.zeros((1, 65), np.float64)
+	    # Specify a region of interest (RoI) and apply grabCut algorithm
 	    rect = (200, 50, 300, 400)
 	    cv2.grabCut(frame, mask, rect, bgdModel, fgdModel, 1, cv2.GC_INIT_WITH_RECT)
+	    # New mask for moving object
 	    mask2 = np.where((mask == 2) | (mask == 0), (0,), (1,)).astype('uint8')
 	    frame = frame * mask2[:, :, np.newaxis]
-
 	    mask_1 = frame > 0
 	    mask_2 = frame <= 0
+	    # Linear combination of bgd and fgd frames with mask_1 and mask_2 "scaliars"
 	    combination = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) * mask_1 + background * mask_2
-
 	    combination = combination.astype(dtype=np.uint8)
-
+	    # Write combined frame
 	    out.write(combination)
 
-	# When everything done, release the capture
+	# When everything done, cleanup the camera and close any open windows
+	print('Background change is finished')
 	cap.release()
 	cv2.destroyAllWindows()
 
@@ -95,14 +130,12 @@ def main():
 	OUTPUT_PATH = config['paths']['OUTPUT_PATH']
 	BG_VIDEO_NAME = config['addition']['BG_VIDEO_NAME']
 
-	background = bg_subtractor(INPUT_PATH + BG_VIDEO_NAME)
+	background = bgSubtractor(INPUT_PATH + BG_VIDEO_NAME)
 
 	for file in os.listdir(INPUT_PATH):
-	    if file.endswith(".mp4"):
+	    if file.endswith('.mp4'):
 	        path=os.path.join(INPUT_PATH, file)
-	        fg_bg_combiner(path, background, OUTPUT_PATH)
-	        break
-
+	        fgBgCombiner(path, background, OUTPUT_PATH)
 
 if __name__ == '__main__':
     main()
